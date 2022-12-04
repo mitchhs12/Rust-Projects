cfa921d425c494abbb68a8566a23cca3249b9872
valodax <snitzel098@gmail.com>
2022-12-04 18:18:28 -0500

    changes to main

--- rust_tut/src/main.rs
+++ rust_tut/src/main.rs
@@ -7,8 +7,10 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    println!("What is your name?");
-    let mut name = String::new();
-    let greeting: &str = "Nice to meet you";
-    println!("Hello {}! {}", name.trim_end(), greeting);
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.14159265;
+    let age = "47";
+    let mut age: u32 = age.trim().parse().expect("Age wasn't assigned a number");
+    age = age + 1;
+    println!("I'm {} and I want ${}", age, ONE_MIL);
 }
